fb576fa85713ae2c346a448595cff8bc24c22ba1
vimalcedric26-alt <vimalcedric26@gmail.com>
2026-02-16 18:06:41 +0530

    Task_12
    commit

---
+++ Task12/Test_dynamic_xpath.py
@@ -0,0 +1,41 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+def test_guvi_navigation(driver):
+    driver.get("https://www.guvi.in/")
+    wait = WebDriverWait(driver, 10)
+
+    # Click Login
+    wait.until(EC.element_to_be_clickable((By.ID, "login-btn"))).click()
+
+    # Enter Email
+    wait.until(EC.visibility_of_element_located((By.ID, "email"))).send_keys("vimalcedric26@gmail.com")
+
+    # Enter Password
+    wait.until(EC.visibility_of_element_located((By.ID, "password"))).send_keys("Cassy@091124")
+
+    # Click Login
+    wait.until(EC.element_to_be_clickable((By.ID, "login-btn"))).click()
+
+    # Navigation
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//p[text()='LIVE Classes']"))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//p[text()='Courses']"))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//p[text()='Practice']"))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//p[text()='Resources']"))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//p[text()='Our Products']"))).click()
+
+    assert "GUVI" in driver.title
